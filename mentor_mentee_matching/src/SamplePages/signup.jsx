057536f4d12f59a4a82b2ff37c3055217842151a
Meron19 <meronamershaoo@gmail.com>
2024-02-20 19:52:22 -0800

    deleted duplicate code

--- mentor_mentee_matching/src/SamplePages/signup.jsx
+++ mentor_mentee_matching/src/SamplePages/signup.jsx
@@ -36,47 +36,26 @@ const SignUpPage = () => {
                     Mentee
                 </button>
             </div>
-            <h1 className="signup-header">Sign Up</h1>
-            <p className ="signup-text">Join SaigonChildren as a</p>
-            <div className="toggle-switch">
-                <button
-                    className={userType === 'mentor' ? 'active' : ''}
-                    onClick={() => handleButtonClick('mentor')}
-                >
-                    Mentor
-                </button>
-                <button
-                    className={userType === 'mentee' ? 'active' : ''}
-                    onClick={() => handleButtonClick('mentee')}
-                >
-                    Mentee
-                </button>
-            </div>
             <form className="signup-form">
                 <div className="form-row">
                     <div className="input-group">
                         <label htmlFor="username">Username</label>
                         <input type="username" id="username" placeholder="Create your username" required />
-                        <label htmlFor="username">Username</label>
-                        <input type="username" id="username" placeholder="Create your username" required />
-                    </div>
+                        </div>
                 </div>
                 <div className="form-row">
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" placeholder="Create your password" required />
-                        <input type="password" id="password" placeholder="Create your password" required />
                     </div>
                 </div>
                 <div className="form-row">
                     <div className="input-group">
                         <label htmlFor="confirmPassword">Confirm Your Password</label>
                         <input type="password" id="confirmPassword" placeholder="Re-enter your password" required />
-                        <label htmlFor="confirmPassword">Confirm Your Password</label>
-                        <input type="password" id="confirmPassword" placeholder="Re-enter your password" required />
                     </div>
                 </div>
-                <button type="submit" className="signup-button">Sign Up</button>
+
                 <button type="submit" className="signup-button">Sign Up</button>
             </form>
             <img src={signupimg} alt="SignupIMG" className="signup-image" />
