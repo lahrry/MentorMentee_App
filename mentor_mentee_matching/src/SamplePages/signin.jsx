42d204bc9a082b693f3fc4f8107a578e00f38984
vesalcido <vesalcido@ucsd.edu>
2024-02-21 14:26:25 -0800

    added both svg files

--- mentor_mentee_matching/src/SamplePages/signin.jsx
+++ mentor_mentee_matching/src/SamplePages/signin.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./signin.css";
-<<<<<<< Updated upstream
+//<<<<<<< Updated upstream
 import saigonSVG from "../saigon.svg";
 import saigon3SVG from "../saigon3.svg";
-=======
->>>>>>> Stashed changes
+//=======
+//>>>>>>> Stashed changes
 
 const SignInPage = () => {
   const navigate = useNavigate();
@@ -18,20 +18,14 @@ const SignInPage = () => {
   return (
     <div className="signin-container">
       <div className="signin-header">
-<<<<<<< Updated upstream
         {/* <img src="/path-to-your-logo.png" alt="Logo" className="signin-logo" /> */}
         {/* <h1>Log In</h1> */}
         <img src={saigon3SVG} alt="Saigon3" className="saigon3-svg" />
-=======
-        <img src="/path-to-your-logo.png" alt="Logo" className="signin-logo" />
-        <h1>Welcome to Saigon Children's Charity</h1>
->>>>>>> Stashed changes
       </div>
       <form className="signin-form" onSubmit={handleSubmit}>
         {" "}
         {/*Add onSubmit handler*/}
         <div className="input-group">
-<<<<<<< Updated upstream
           {/* <label htmlFor="email">Username</label>
           <input type="email" id="email" required /> */}
         </div>
@@ -47,19 +41,6 @@ const SignInPage = () => {
         <img src={saigonSVG} alt="Saigon" className="saigon-svg" />
       </div>
       {/* <img src={saigonSVG} alt="Saigon" className="saigon-svg" /> */}
-=======
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" required />
-        </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" required />
-        </div>
-        <button type="submit" className="signin-button">
-          Sign in
-        </button>
-      </form>
->>>>>>> Stashed changes
     </div>
   );
 };
