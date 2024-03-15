1b56cb0ca27f1f12b62a21f8d2311ee7fab7927e
Bella Jeong <lahrry0925@gmail.com>
2024-03-14 17:55:15 -0700

    landing page

--- mentor_mentee_matching/src/App.js
+++ mentor_mentee_matching/src/App.js
@@ -1,40 +1,40 @@
-// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-// import './App.css';
-// import LandingPage from './SamplePages/landing.jsx';
-// import SignInPage from './SamplePages/signin.jsx';
-// import SignUpPage from './SamplePages/signup.jsx';
-// import Website from './SamplePages/website.jsx';
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<LandingPage />} />
-//         <Route path="/login" element={<SignInPage />} />
-//         <Route path="/signup" element={<SignUpPage />} />
-//         <Route path="/website" element={<Website />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-//{Showing the main website first when we access}
-import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './SamplePages/landing.jsx';
+import SignInPage from './SamplePages/signin.jsx';
 import SignUpPage from './SamplePages/signup.jsx';
+import Website from './SamplePages/website.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<SignUpPage />} /> 
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/website" element={<Website />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+//{Showing the main website first when we access}
+// import React from 'react';
+// import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// import './App.css';
+// import SignUpPage from './SamplePages/signup.jsx';
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<SignUpPage />} /> 
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
 
